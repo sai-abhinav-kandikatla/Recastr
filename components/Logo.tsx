64a4cea2ee70a62ffdf8b78a3460172f02444df8
saiabhinavkandikatla-glitch <saiabhinavkandikatla@gmail.com>
2026-06-20 11:41:23 +0530

    style: replace logo mark R path with modern bold sans-serif design

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -25,7 +25,7 @@ export function LogoMark({ size = 28 }: { size?: number }) {
     <svg width={size} height={size} viewBox="0 0 32 32" fill="none" aria-hidden="true">
       <rect width="32" height="32" rx="8" fill="#ffffff" />
       <path
-        d="M 9 7 H 18 C 21.5 7, 23 8.5, 23 11 C 23 13.5, 21.5 15, 18 15 H 13.5 V 25 H 9 Z M 13.5 10 V 12 H 17.5 C 18.5 12, 19.5 11.5, 19.5 11 C 19.5 10.5, 18.5 10, 17.5 10 Z M 13.5 15 H 18.5 L 22.5 25 H 18 Z"
+        d="M 9 8 H 19.5 C 21.5 8, 23 9.5, 23 12 C 23 14.5, 21.5 16, 19.5 16 H 13 V 24 H 9 Z M 13 11 H 19 C 20 11, 20.5 11.5, 20.5 12.5 C 20.5 13.5, 20 14, 19 14 H 13 Z M 15 16 H 19 L 23.5 24 H 19.5 L 15.5 17 Z"
         fill="#09090b"
         fillRule="evenodd"
         clipRule="evenodd"
